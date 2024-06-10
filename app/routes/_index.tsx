9af3f89abc03fb01241ef47da6ff3182b02b73af
Mazen Zibara <mazen@funded.app>
2024-06-10 13:39:38 +0200

    Add first call

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,41 @@
-import type { MetaFunction } from "@remix-run/node";
+import { useEffect, useState, useCallback } from 'react';
 
-export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+import { Principal } from '@dfinity/principal'
+
+import { idlFactory } from '../idls/icrc2.did';
 
 export default function Index() {
+  const [actor, setActor] = useState()
+
+  const connect = useCallback(async () => {
+    await window.ic.plug.requestConnect({ whitelist: ['zc5ct-kaaaa-aaaah-qddga-cai'] })
+    const actor = await window.ic.plug.createActor({ interfaceFactory: idlFactory, canisterId: 'zc5ct-kaaaa-aaaah-qddga-cai' })
+    setActor(actor)
+  }, [])
+
+  const callApprove = useCallback(() => {
+    if (!actor) return Promise.reject('No actor')
+
+
+    return actor.icrc2_approve({
+      spender: {
+        owner: Principal.from('e344b-tjf6e-yey57-q2mkx-jkpnl-232ia-lfnfj-6kqvy-75vtv-6dlgk-lqe'),
+        subaccount: []
+      },
+      amount: 10000n,
+      fee: [1n],
+      memo: [],
+      from_subaccount: [],
+      created_at_time: [],
+      expected_allowance: [],
+      expires_at: []
+    })
+  }, [actor])
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <button onClick={() => connect().catch(console.log)}>Connect</button>
+      <button onClick={() => callApprove().catch(console.log)}>APPROVE</button>
     </div>
   );
 }
